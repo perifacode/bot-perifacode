4eb2db33f1b19f2f24f3cef4e098460b7916fe1f
gitlherme <glvs011@gmail.com>
2020-03-26 19:01:57 -0300

    create coursesAndBooks command

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { Client } = require('discord.js')
+const { Client, MessageEmbed } = require('discord.js')
 const client = new Client()
 const getEvents = require('./commands/events')
 const greetings = require('./commands/greeting')
+const coursesAndBooks = require('./commands/coursesAndBooks')
 require('dotenv/config')
 
 const base_url = "https://api.sympla.com.br/public/v3/events"
@@ -13,6 +14,8 @@ client.on('ready', () => {
 
 // !eventos - Mostra o próximo evento do perifaCode
 client.on('message', msg => {
+  greetings(msg)
+  coursesAndBooks(msg)
   if(msg.content === '!eventos') {
     getEvents(base_url)
     .then(nextEvent => {
@@ -23,12 +26,5 @@ client.on('message', msg => {
     })
   }
 })
-
-
-// Saudações (Bom dia, boa tarde e boa noite)
-client.on('message', msg => {
-  greetings(msg)
-})
-
 
 client.login(process.env.TOKEN_DISCORD)
