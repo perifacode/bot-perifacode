62df19ec89d0df6ffbe8ad1664b43874d5e23a50
gitlherme <glvs011@gmail.com>
2020-04-01 23:26:48 -0300

    Adptando código existe para o ESLint

--- commands/greeting.js
+++ commands/greeting.js
@@ -1,17 +1,17 @@
 module.exports = function greetings(msg) {
-  if(msg.content === 'bom dia' || msg.content === 'Bom dia'  || msg.content === 'Bom dia!' || msg.content === 'bom dia!') {
-    msg.channel.send('Bom diaaaa! 🌞')
-    msg.react('🌞')
+  if (msg.content === 'bom dia' || msg.content === 'Bom dia' || msg.content === 'Bom dia!' || msg.content === 'bom dia!') {
+    msg.channel.send('Bom diaaaa! 🌞');
+    msg.react('🌞');
   }
 
-  if(msg.content === 'boa tarde' || msg.content === 'Boa tarde'  || msg.content === 'Boa tarde!' || msg.content === 'boa tarde!') {
-    msg.channel.send('Boa tardeeee! 🌤')
-    msg.react('⛅')
+  if (msg.content === 'boa tarde' || msg.content === 'Boa tarde' || msg.content === 'Boa tarde!' || msg.content === 'boa tarde!') {
+    msg.channel.send('Boa tardeeee! 🌤');
+    msg.react('⛅');
   }
 
-  if(msg.content === 'boa noite' || msg.content === 'Boa noite' || msg.content === 'Boa noite!' || msg.content === 'boa noite!') {
-    msg.channel.send('Boa noiteee! 😴')
-    msg.react('😴')
-    msg.react('💤')
+  if (msg.content === 'boa noite' || msg.content === 'Boa noite' || msg.content === 'Boa noite!' || msg.content === 'boa noite!') {
+    msg.channel.send('Boa noiteee! 😴');
+    msg.react('😴');
+    msg.react('💤');
   }
-}
+};
--- commands/site.js
+++ commands/site.js
@@ -1,12 +1,12 @@
-const { MessageEmbed } = require ('discord.js')
+const { MessageEmbed } = require('discord.js');
 
 module.exports = function site(msg) {
-  if(msg.content === '!site') {
-    embed = new MessageEmbed()
+  if (msg.content === '!site') {
+    const embed = new MessageEmbed()
       .setTitle(' 💻 Se liga no site da nossa comunidade 💻')
       .setDescription('🔗 http://perifacode.com')
-      .setFooter(`Criado com 💛 pela comunidade perifaCode`)
-      .setColor('#fff200')
-    msg.channel.send(embed)
+      .setFooter('Criado com 💛 pela comunidade perifaCode')
+      .setColor('#fff200');
+    msg.channel.send(embed);
   }
-}
+};
