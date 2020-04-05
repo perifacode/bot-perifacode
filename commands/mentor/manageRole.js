922ee0126612938676437e0797aa647acfb08ad8
Lucas Silva <lukells@outlook.com>
2020-04-05 00:05:46 -0300

    função que adiciona ou remove um papel de um member

---
+++ commands/mentor/manageRole.js
@@ -0,0 +1,14 @@
+const roles = require('../../roles');
+
+module.exports = function(reactionsList, guildMember, rolesList) {
+  const guildMemberRoles = guildMember.roles._roles;
+  reactionsList.forEach(reaction => {
+    const role = roles.filter(role => role.emoji === reaction)[0];
+    const guildRole = rolesList[0].cache.find(r => r.name.toLowerCase() === role.roleName);
+    if(guildMemberRoles.find(role => role.name === guildRole.name) && reaction !== "✅"){
+      guildMember.roles.remove(guildRole.id)
+    } else {
+      guildMember.roles.add(guildRole.id);
+    }
+  })
+}
