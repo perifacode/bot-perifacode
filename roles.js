5da76a962e76e3189f0e4062eecccd1a770fa690
Lucas Silva <lukells@outlook.com>
2020-10-01 12:57:13 -0300

    corrige eslint

--- roles.js
+++ roles.js
@@ -1,60 +1,60 @@
 const roles = [
-	{
-		roleName: "front-end",
-		emoji: '0️⃣'
-	},
-	{
-		roleName: "back-end",
-		emoji: '1️⃣'
-	},
-	{
-		roleName: "data science",
-		emoji: '2️⃣'
-	},
-	{
-		roleName: "javascript",
-		emoji: '3️⃣'
-	},
-	{
-		roleName: "java",
-		emoji: '4️⃣'
-	},
-	{
-		roleName: "python",
-		emoji: '5️⃣'
-	},
-	{
-		roleName: "c#",
-		emoji: '6️⃣'
-	},
-	{
-		roleName: "android",
-		emoji: '7️⃣'
-	},
-	{
-		roleName: "nodejs",
-		emoji: '8️⃣'
-	},
-	{
-		roleName: "ruby",
-		emoji: '9️⃣'
-	},
-	{
-		roleName: "devops",
-		emoji: '🔟'
-	},
-	{
-		roleName: "gamedev",
-		emoji: '🎮'
-	},
-	{
-		roleName: "mentor",
-		emoji: '✅'
-	},
-	{
-		roleName: "mentorado",
-		emoji: '🟢'
-	}
-]
+  {
+    roleName: 'front-end',
+    emoji: '0️⃣',
+  },
+  {
+    roleName: 'back-end',
+    emoji: '1️⃣',
+  },
+  {
+    roleName: 'data science',
+    emoji: '2️⃣',
+  },
+  {
+    roleName: 'javascript',
+    emoji: '3️⃣',
+  },
+  {
+    roleName: 'java',
+    emoji: '4️⃣',
+  },
+  {
+    roleName: 'python',
+    emoji: '5️⃣',
+  },
+  {
+    roleName: 'c#',
+    emoji: '6️⃣',
+  },
+  {
+    roleName: 'android',
+    emoji: '7️⃣',
+  },
+  {
+    roleName: 'nodejs',
+    emoji: '8️⃣',
+  },
+  {
+    roleName: 'ruby',
+    emoji: '9️⃣',
+  },
+  {
+    roleName: 'devops',
+    emoji: '🔟',
+  },
+  {
+    roleName: 'gamedev',
+    emoji: '🎮',
+  },
+  {
+    roleName: 'mentor',
+    emoji: '✅',
+  },
+  {
+    roleName: 'mentorado',
+    emoji: '🟢',
+  },
+];
 
 module.exports = roles;
